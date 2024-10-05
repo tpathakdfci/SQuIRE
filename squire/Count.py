0c0932f3f44c97126ff1f0a406e84f77c061bc90
tpathakdfci <148574945+tpathakdfci@users.noreply.github.com>
2024-10-05 18:01:12 -0400

    Update Count.py

--- squire/Count.py
+++ squire/Count.py
@@ -40,7 +40,7 @@ def get_basename(filepath):
 def make_dir(path):
 	try:
 		original_umask = os.umask(0)
-		os.makedirs(path, 0770)
+		os.makedirs(path, 0o770)
 	except OSError as exception:
 		if exception.errno != errno.EEXIST:
 			raise
